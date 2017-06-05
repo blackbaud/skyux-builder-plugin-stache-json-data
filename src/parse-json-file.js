56b958b933bf0106732abb09ce1696f2d101bfcb
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-06-05 12:58:34 -0400

    Renamed module

--- src/parse-json-file.js
+++ src/parse-json-file.js
@@ -1,42 +1,51 @@
 const fs = require('fs');
 const path = require('path');
 const root = path.resolve(process.cwd(), 'src', 'stache', 'data');
+const cheerio = require('cheerio');
+
+// Adds directive and template reference variable to stache tag:
+// <stache #stache="jsonData" stacheJsonData></stache>
+function addTemplateReferenceVariable(content) {
+  let $ = cheerio.load(content, {
+    lowerCaseTags: false,
+    lowerCaseAttributeNames: false,
+    decodeEntities: false
+  });
+
+  let stacheTags = $('stache');
+
+  if (stacheTags.length) {
+    stacheTags.each(function () {
+      $(this)
+        .attr('stacheJsonData', '')
+        .attr('#stache', 'jsonData');
+    });
+
+    content = $.html().toString();
+  }
+
+  return content;
+}
 
 const preload = (content, resourcePath) => {
-  if (!resourcePath.match(/src\/modules\/shared\/data\.service\.ts$/)) {
+  if (resourcePath.match(/\.html$/)) {
+    return addTemplateReferenceVariable(content);
+  }
+
+  if (!resourcePath.match(/src\/modules\/json-data\/json-data\.service\.ts$/)) {
     return content;
   }
 
   let files = fs.readdirSync(root);
 
-  let dataObject = files.reduce(function (acc, file) {
+  let dataObject = files.reduce((acc, file) => {
     let fileContent = fs.readFileSync(path.join(root, file), 'utf8');
     let fileName = file.split('.')[0];
     acc[fileName] = JSON.parse(fileContent);
     return acc;
   }, {});
 
-  let template = `
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StacheDataService {
-  /* tslint:disable */
-  private data: any = ${JSON.stringify(dataObject)};
-  /* tslint:enable */
-
-  public getAllData(): any {
-    return this.data;
-  }
-
-  public getData(key: string): any {
-    return this.data[key];
-  }
-}
-`;
-
-  content = template;
-
+  content = content.replace(`'noop'`, JSON.stringify(dataObject));
 
   return content;
 };
