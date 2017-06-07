ec1bd327e4f10c913d2c1526c046ed01bff01be4
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2017-06-07 09:22:36 -0400

    Simplified tests.

--- src/json-data.spec.js
+++ src/json-data.spec.js
@@ -11,25 +11,7 @@ describe('JSON Data Plugin', () => {
         'test file.json': '{ "test":"test" }'
       },
       'foo.html': '<stache></stache>',
-      'shared/json-data.service.ts': `import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StacheJsonDataService {
-  // The noop gets populated automatically by JSON file contents.
-  // https://github.com/blackbaud/skyux-builder-plugin-stache-parse-json-file
-  // (Disabling TSLint for this line because the file contents does not conform to linting rules.)
-  /* tslint:disable:quotemark whitespace */
-  private jsonData: any = 'noop';
-  /* tslint:enable:quotemark whitespace */
-
-  public getAll(): any {
-    return this.jsonData;
-  }
-
-  public getFileData(fileName: string) {
-    return this.jsonData[key];
-  }
-}`
+      'shared/json-data.service.ts': `private jsonData: any = 'noop';`
     });
   });
   afterAll(() => {
@@ -62,90 +44,16 @@ export class StacheJsonDataService {
   });
 
   it('should replace the noop string in the json-data service file with the json data.', () => {
-    const content = `import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StacheJsonDataService {
-  // The noop gets populated automatically by JSON file contents.
-  // https://github.com/blackbaud/skyux-builder-plugin-stache-parse-json-file
-  // (Disabling TSLint for this line because the file contents does not conform to linting rules.)
-  /* tslint:disable:quotemark whitespace */
-  private jsonData: any = 'noop';
-  /* tslint:enable:quotemark whitespace */
-
-  public getAll(): any {
-    return this.jsonData;
-  }
-
-  public getFileData(fileName: string) {
-    return this.jsonData[key];
-  }
-}`;
-
-    const contentParsed = `import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StacheJsonDataService {
-  // The noop gets populated automatically by JSON file contents.
-  // https://github.com/blackbaud/skyux-builder-plugin-stache-parse-json-file
-  // (Disabling TSLint for this line because the file contents does not conform to linting rules.)
-  /* tslint:disable:quotemark whitespace */
-  private jsonData: any = {"config":{"productNameLong":"Stache 2"},"test-file":{"test":"test"}};
-  /* tslint:enable:quotemark whitespace */
-
-  public getAll(): any {
-    return this.jsonData;
-  }
-
-  public getFileData(fileName: string) {
-    return this.jsonData[key];
-  }
-}`;
+    const content = `private jsonData: any = 'noop';`;
+    const contentParsed = `private jsonData: any = {"config":{"productNameLong":"Stache 2"},"test-file":{"test":"test"}};`;
     const path = 'shared/json-data.service.ts';
     let result = plugin.preload(content, path);
     expect(result).toContain(contentParsed);
   });
 
   it('should create a valid object key given a file name.', () => {
-    const content = `import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StacheJsonDataService {
-  // The noop gets populated automatically by JSON file contents.
-  // https://github.com/blackbaud/skyux-builder-plugin-stache-parse-json-file
-  // (Disabling TSLint for this line because the file contents does not conform to linting rules.)
-  /* tslint:disable:quotemark whitespace */
-  private jsonData: any = {"config":{"productNameLong":"Stache 2"},"test-file":{"test":"test"}};
-  /* tslint:enable:quotemark whitespace */
-
-  public getAll(): any {
-    return this.jsonData;
-  }
-
-  public getFileData(fileName: string) {
-    return this.jsonData[key];
-  }
-}`;
-
-    const parsedContent = `import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StacheJsonDataService {
-  // The noop gets populated automatically by JSON file contents.
-  // https://github.com/blackbaud/skyux-builder-plugin-stache-parse-json-file
-  // (Disabling TSLint for this line because the file contents does not conform to linting rules.)
-  /* tslint:disable:quotemark whitespace */
-  private jsonData: any = {"config":{"productNameLong":"Stache 2"},"test-file":{"test":"test"}};
-  /* tslint:enable:quotemark whitespace */
-
-  public getAll(): any {
-    return this.jsonData;
-  }
-
-  public getFileData(fileName: string) {
-    return this.jsonData[key];
-  }
-}`;
+    const content = `private jsonData: any = {"config":{"productNameLong":"Stache 2"},"test-file":{"test":"test"}};`;
+    const parsedContent = `private jsonData: any = {"config":{"productNameLong":"Stache 2"},"test-file":{"test":"test"}};`;
     const path = 'shared/json-data.service.ts';
     let result = plugin.preload(content,path);
     expect(result).toContain(parsedContent);
@@ -157,50 +65,14 @@ describe('JSON Data plugin no files.', () => {
     mock({
       'src/stache/data': {
       },
-      'shared/json-data.service.ts': `import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StacheJsonDataService {
-  // The noop gets populated automatically by JSON file contents.
-  // https://github.com/blackbaud/skyux-builder-plugin-stache-parse-json-file
-  // (Disabling TSLint for this line because the file contents does not conform to linting rules.)
-  /* tslint:disable:quotemark whitespace */
-  private jsonData: any = 'noop';
-  /* tslint:enable:quotemark whitespace */
-
-  public getAll(): any {
-    return this.jsonData;
-  }
-
-  public getFileData(fileName: string) {
-    return this.jsonData[key];
-  }
-}`
+      'shared/json-data.service.ts': `private jsonData: any = 'noop';`
     });
   });
   afterAll(() => {
     mock.restore();
   });
   it('should return the data service file unchanged if no json files exist.', () => {
-    const content = `import { Injectable } from '@angular/core';
-
-@Injectable()
-export class StacheJsonDataService {
-  // The noop gets populated automatically by JSON file contents.
-  // https://github.com/blackbaud/skyux-builder-plugin-stache-parse-json-file
-  // (Disabling TSLint for this line because the file contents does not conform to linting rules.)
-  /* tslint:disable:quotemark whitespace */
-  private jsonData: any = 'noop';
-  /* tslint:enable:quotemark whitespace */
-
-  public getAll(): any {
-    return this.jsonData;
-  }
-
-  public getFileData(fileName: string) {
-    return this.jsonData[key];
-  }
-}`;
+    const content = `private jsonData: any = 'noop';`;
     const path = 'src/shared/json-data.service.ts';
     let result = plugin.preload(content, path);
     expect(result).toBe(content);
