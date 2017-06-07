063ca6f9b2c4b354578f509bcd0976ed7ffebe63
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-06-07 12:49:26 -0400

    Updated unit tests

--- src/json-data.js
+++ src/json-data.js
@@ -1,36 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const root = path.resolve(process.cwd(), 'src', 'stache', 'data');
-const cheerio = require('cheerio');
+const reserved = require('reserved-words');
 
-// Adds template reference variable to stache tag:
-// <stache #stache></stache>
-function addTemplateReferenceVariable(content) {
-  let $ = cheerio.load(content, {
-    lowerCaseTags: false,
-    lowerCaseAttributeNames: false,
-    decodeEntities: false
-  });
-
-  let stacheTags = $('stache');
-
-  if (stacheTags.length) {
-    stacheTags.each(function () {
-      $(this)
-        .attr('#stache', '');
-    });
-
-    content = $.html().toString();
+const isPropertyNameValid = (propertyName) => {
+  if (!propertyName) {
+    return false;
   }
 
-  return content;
-}
+  if (propertyName === 'prototype') {
+    return false;
+  }
+
+  // Parsing as boolean because reserved-words returns `undefined` for falsy values.
+  return !reserved.check(propertyName, 'es6', true);
+};
 
 const preload = (content, resourcePath) => {
-  if (resourcePath.match(/\.html$/)) {
-    return addTemplateReferenceVariable(content);
-  }
-
   if (!resourcePath.match(/shared\/json-data\.service\.ts$/)) {
     return content;
   }
@@ -38,18 +24,29 @@ const preload = (content, resourcePath) => {
   let files = fs.readdirSync(root);
 
   if (files.length > 0) {
-
     let dataObject = files.reduce((acc, file) => {
-      let fileContent = fs.readFileSync(path.join(root, file), 'utf8');
-      let fileName = file.split('.')[0].toLowerCase()
+      const filePath = path.join(root, file);
+      let fileContent = fs.readFileSync(filePath, 'utf8');
+      let propertyName = file.split('.')[0]
+        .toLowerCase()
         .replace(/\s+/g, '-')        // Replace spaces with -
         .replace(/[^\w\-]+/g, '')    // Remove all non-word chars
+        .replace(/_/g, '-')          // Replace all underscores with -
         .replace(/\-\-+/g, '-')      // Replace multiple - with single -
         .replace(/^-+/, '')          // Trim - from start of text
         .replace(/-+$/, '');         // Trim - from end of text;
-      acc[fileName] = JSON.parse(fileContent);
+
+      if (!isPropertyNameValid(propertyName)) {
+        console.error(
+          `[SKY UX Plugin Error: stache-json-data] A valid Object property could not be determined from file ${filePath}!`
+        );
+        return acc;
+      }
+
+      acc[propertyName] = JSON.parse(fileContent);
+
       return acc;
-    }, {});
+    }, { });
 
     content = content.replace(`'noop'`, JSON.stringify(dataObject));
   }
--- src/json-data.spec.js
+++ src/json-data.spec.js
@@ -1,5 +1,3 @@
-/* node: true */
-
 const plugin = require('./json-data');
 const mock = require('mock-fs');
 
@@ -10,10 +8,10 @@ describe('JSON Data Plugin', () => {
         'config.json': '{ "productNameLong": "Stache 2" }',
         'test file.json': '{ "test":"test" }'
       },
-      'foo.html': '<stache></stache>',
       'shared/json-data.service.ts': `private jsonData: any = 'noop';`
     });
   });
+
   afterAll(() => {
     mock.restore();
   });
@@ -22,23 +20,9 @@ describe('JSON Data Plugin', () => {
     expect(plugin.preload).toBeDefined();
   });
 
-  it('should not alter the content if the resourcePath is not an html file.', () => {
+  it('should not alter the content if the resourcePath is not the json-data service file.', () => {
     const content = 'let foo = "bar";';
     const path = 'foo.js';
-    let result = plugin.preload(content, path);
-    expect(result).toBe(content);
-  });
-
-  it('should add the template reference variable to each <stache> tag', () => {
-    const content = '<stache></stache>';
-    const path = 'foo.html';
-    let result = plugin.preload(content, path);
-    expect(result).toBe('<stache #stache=""></stache>');
-  });
-
-  it('should not add the attribute if no stache tag exists', () => {
-    const content = '<h1>No attribute</h1>';
-    const path = 'foo.html';
     let result = plugin.preload(content, path);
     expect(result).toBe(content);
   });
@@ -52,27 +36,60 @@ describe('JSON Data Plugin', () => {
   });
 
   it('should create a valid object key given a file name.', () => {
-    const content = `private jsonData: any = {"config":{"productNameLong":"Stache 2"},"test-file":{"test":"test"}};`;
-    const parsedContent = `private jsonData: any = {"config":{"productNameLong":"Stache 2"},"test-file":{"test":"test"}};`;
-    const path = 'shared/json-data.service.ts';
-    let result = plugin.preload(content,path);
-    expect(result).toContain(parsedContent);
-  });
-});
-
-describe('JSON Data plugin no files.', () => {
-  beforeAll(() => {
+    const content = `'noop'`;
     mock({
       'src/stache/data': {
+        'config.json': '{ }',
+        'file with spaces.json': '{ }',
+        'Tes*$^^@*(@$tfile.json': '{ }',
+        'file with UPPERCASE LETTERS.json': '{ }',
+        'file w1th n0mb3rs.json': '{ }',
+        'file-with-dashes.json': '{ }',
+        'file_with_underscores_____.json': '{ }',
+        '__proto__.json': '{ }'
       },
-      'shared/json-data.service.ts': `private jsonData: any = 'noop';`
+      'shared/json-data.service.ts': content
     });
+    const path = 'src/shared/json-data.service.ts';
+    let result = plugin.preload(content, path);
+
+    expect(result).toContain('"config"');
+    expect(result).toContain('"file-with-spaces"');
+    expect(result).toContain('"testfile"');
+    expect(result).toContain('"file-with-uppercase-letters"');
+    expect(result).toContain('"file-w1th-n0mb3rs"');
+    expect(result).toContain('"file-with-dashes"');
+    expect(result).toContain('"file-with-underscores"');
+    expect(result).toContain('"proto"');
   });
-  afterAll(() => {
-    mock.restore();
+
+  it('should handle invalid file names.', () => {
+    spyOn(console, 'error');
+
+    const content = `'noop'`;
+    const path = 'src/shared/json-data.service.ts';
+    const invalidFiles = {
+      '.json': '{ }',
+      '*****.json': '{ }',
+      'constructor': '{ }',
+      'prototype': '{ }',
+      'テナンス中です': '{ }'
+    };
+
+    mock({
+      'src/stache/data': invalidFiles
+    });
+
+    plugin.preload(content, path);
+    expect(console.error.calls.count()).toBe(Object.keys(invalidFiles).length);
   });
+
   it('should return the data service file unchanged if no json files exist.', () => {
-    const content = `private jsonData: any = 'noop';`;
+    const content = `'noop'`;
+    mock({
+      'src/stache/data': { },
+      'shared/json-data.service.ts': content
+    });
     const path = 'src/shared/json-data.service.ts';
     let result = plugin.preload(content, path);
     expect(result).toBe(content);
