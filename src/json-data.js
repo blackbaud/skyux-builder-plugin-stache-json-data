c7a7641cca4e70d89fed36c1de8b1cfb83b69432
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-06-08 16:45:26 -0400

    Added providers

--- src/json-data.js
+++ src/json-data.js
@@ -3,6 +3,59 @@ const path = require('path');
 const root = path.resolve(process.cwd(), 'src', 'stache', 'data');
 const reserved = require('reserved-words');
 
+const preload = (content, resourcePath) => {
+  if (!resourcePath.match(/stache-extras\.module\.ts$/)) {
+    return content;
+  }
+
+  let files = fs.readdirSync(root);
+
+  if (!files.length) {
+    return content;
+  }
+
+  let dataObject = files.reduce((acc, file) => {
+    const filePath = path.join(root, file);
+    let fileContent = fs.readFileSync(filePath, 'utf8');
+    let propertyName = convertFileNameToObjectPropertyName(file);
+
+    if (!isPropertyNameValid(propertyName)) {
+      console.error(
+        `[SKY UX Plugin Error: stache-json-data] A valid Object property could not be determined from file ${filePath}!`
+      );
+      return acc;
+    }
+
+    acc[propertyName] = JSON.parse(fileContent);
+
+    return acc;
+  }, { });
+
+  return `
+import {
+  STACHE_JSON_DATA_SERVICE_CONFIG_TOKEN,
+  STACHE_JSON_DATA_PROVIDERS
+} from '@blackbaud/stache';
+
+STACHE_JSON_DATA_PROVIDERS.push({
+  provide: STACHE_JSON_DATA_SERVICE_CONFIG_TOKEN,
+  useValue: ${JSON.stringify(dataObject)}
+});
+
+${content}`;
+};
+
+const convertFileNameToObjectPropertyName = (fileName) => {
+  return fileName.split('.')[0]
+    .toLowerCase()
+    .replace(/\s+/g, '-')        // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')    // Remove all non-word chars
+    .replace(/_/g, '-')          // Replace all underscores with -
+    .replace(/\-\-+/g, '-')      // Replace multiple - with single -
+    .replace(/^-+/, '')          // Trim - from start of text
+    .replace(/-+$/, '');         // Trim - from end of text;
+};
+
 const isPropertyNameValid = (propertyName) => {
   if (!propertyName) {
     return false;
@@ -14,44 +67,6 @@ const isPropertyNameValid = (propertyName) => {
 
   // Parsing as boolean because reserved-words returns `undefined` for falsy values.
   return !reserved.check(propertyName, 'es6', true);
-};
-
-const preload = (content, resourcePath) => {
-  if (!resourcePath.match(/shared\/json-data\.service\.ts$/)) {
-    return content;
-  }
-
-  let files = fs.readdirSync(root);
-
-  if (files.length > 0) {
-    let dataObject = files.reduce((acc, file) => {
-      const filePath = path.join(root, file);
-      let fileContent = fs.readFileSync(filePath, 'utf8');
-      let propertyName = file.split('.')[0]
-        .toLowerCase()
-        .replace(/\s+/g, '-')        // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')    // Remove all non-word chars
-        .replace(/_/g, '-')          // Replace all underscores with -
-        .replace(/\-\-+/g, '-')      // Replace multiple - with single -
-        .replace(/^-+/, '')          // Trim - from start of text
-        .replace(/-+$/, '');         // Trim - from end of text;
-
-      if (!isPropertyNameValid(propertyName)) {
-        console.error(
-          `[SKY UX Plugin Error: stache-json-data] A valid Object property could not be determined from file ${filePath}!`
-        );
-        return acc;
-      }
-
-      acc[propertyName] = JSON.parse(fileContent);
-
-      return acc;
-    }, { });
-
-    content = content.replace(`'noop'`, JSON.stringify(dataObject));
-  }
-
-  return content;
 };
 
 module.exports = { preload };
